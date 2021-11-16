b94bf63e37e1250b919b0163db17761fbd775951
joaojoao2105 <94456676+joaojoao2105@users.noreply.github.com>
2021-11-16 11:46:43 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+const botao = document.getElementById("btnClique")
+
+botao.addEventListener('click', Função)
+
+function Função(){
+    alert("Olá, o meu número é (##) #####-#### e meu e-mail é dev*codenapp.com, pode enviar qualquer coisa que estarei disponível até as ##h")
+    return
+
+    alert
+}
